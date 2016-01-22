c82121f5c0125e0a298d9aa45f41df6188640fde
Paris Holley <mail@parisholley.com>
2016-01-21 18:18:37 -0800

    New location for video assets

--- html/video.php
+++ html/video.php
@@ -1,18 +1,12 @@
 <div id="mantis_player"></div>
 
-<script type="text/javascript" data-cfasync="false">
-	var MANTIS_VIDEO = {
+<script type="text/javascript">
+	var mantis = mantis || [];
+	mantis.push(['video', 'load', {
 		container: 'mantis_player',
 		property: '<?php echo $property ?>',
 		videoId: '<?php echo $id ?>'
-	};
+	}]);
 </script>
 
-<script type="text/javascript" data-cfasync="false">
-	var z = document.createElement("script");
-	z.type = "text/javascript";
-	z.async = true;
-	z.src = "//assets.mantisadnetwork.com/video.min.js";
-	var s = document.getElementsByTagName('head')[0];
-	s.parentNode.insertBefore(z, s);
-</script>
+<script type="text/javascript" src="https://assets.mantisadnetwork.com/video/video.min.js" async></script>
