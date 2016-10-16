8434d9b35c8c2603289ae7c73a9dddef3c4760d9
Paris Holley <mail@parisholley.com>
2016-10-16 17:34:23 -0400

    Do not insert content recommendation widget on homepage

--- recommend.php
+++ recommend.php
@@ -2,55 +2,59 @@
 
 function mantis_recommend_html()
 {
-    ob_start();
+	ob_start();
 
-    $site = get_option('mantis_site_id');
+	$site = get_option('mantis_site_id');
 
-    require(MANTIS_ROOT . '/html/recommend.php');
+	require(MANTIS_ROOT . '/html/recommend.php');
 
-    $html = ob_get_contents();
+	$html = ob_get_contents();
 
-    ob_end_clean();
+	ob_end_clean();
 
-    return $html;
+	return $html;
 }
 
 function mantis_recommend_comments($file)
 {
-    global $mantis_comments;
+	global $mantis_comments;
 
-    if($mantis_comments){
-        return $mantis_comments;
-    }
+	if ($mantis_comments) {
+		return $mantis_comments;
+	}
 
-    $mantis_comments = $file;
+	$mantis_comments = $file;
 
-    return MANTIS_ROOT . '/comments.php';
+	return MANTIS_ROOT . '/comments.php';
 }
 
 function mantis_recommend_after($content)
 {
-    return $content . mantis_recommend_html();
+	if (is_home()) {
+		return $content;
+	}
+
+	return $content . mantis_recommend_html();
 }
 
 function mantis_recommend_render()
 {
-    echo mantis_recommend_html();
+	echo mantis_recommend_html();
 }
 
 function mantis_recommend()
 {
-    $location = get_option('mantis_recommend');
+	$location = get_option('mantis_recommend');
 
-    switch ($location) {
-        case 'after_content':
-            add_filter('the_content', 'mantis_recommend_after', 2);
-            break;
-        case 'before_comments':
-        case 'after_comments':
-            add_filter('comments_template', 'mantis_recommend_comments');
-            break;
-    }
+	switch ($location) {
+		case 'after_content':
+			add_filter('the_content', 'mantis_recommend_after', 2);
+			break;
+		case 'before_comments':
+		case 'after_comments':
+			add_filter('comments_template', 'mantis_recommend_comments');
+			break;
+	}
 }
 
 add_action('init', 'mantis_recommend');
